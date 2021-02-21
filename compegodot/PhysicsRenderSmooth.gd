4a90f35316fd8d41afdd3239d71fa15deb880a7e
tengkuizdihar <tengkuizdihar@gmail.com>
2021-02-21 11:35:05 +0700

    Refactoring and fix crouch bug

---
+++ compegodot/PhysicsRenderSmooth.gd
@@ -0,0 +1,10 @@
+class_name PhysicsRenderSmooth
+extends Spatial
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# TODO: interpolate the children's position based on the current origin.
+#func _process(delta: float) -> void:
+#	pass
--- compegodot/Player.gd
+++ compegodot/Player.gd
@@ -10,17 +10,18 @@ extends KinematicBody
 ###              ramps.
 const MAGIC_ON_GROUND_GRAVITY = 9 # m/s
 
+onready var headlimit_raycast = $HeadLimitRayCast
 onready var normal_input_direction = Vector3.ZERO # will be changed by outside actors
 onready var camera = $Pivot/Camera
 onready var pivot = $Pivot
 onready var mouse_sensitivity = 0.0008  # radians/pixel, TODO: refactor to game settings
-onready var current_acceleration = ground_acceleration
+onready var current_acceleration = GROUND_ACCELERATION
 
 ### A counter that will increase as many times as it jumps until it's on the floor again
 var jump_counter = 0
 
 ### The currently used max velocity for movement input
-var current_max_movement_velocity = max_run_velocity
+var current_max_movement_velocity = MAX_RUN_VELOCITY
 
 ### The current velocity for the gravity applied to the character. Will change based
 ### on the situation. For example it could be the same as MAGIC_ON_GROUND_GRAVITY.
@@ -34,27 +35,32 @@ var final_velocity = Vector3()
 
 ### Flag for crouching
 var is_crouching = false
+var debug_position_one_frame_ago = Vector3.ZERO
 onready var feet_original_local_translation = $Feet.transform.origin
 onready var body_original_local_translation = $Body.transform.origin
 onready var body_original_height = $Body.shape.height
+onready var crouch_height = 1.75
 
-export(bool) var auto_bhop = false
-export(float) var crouch_height = 2 # based on collision body height
-export(float) var crouch_speed = 3 # meter per second
-export(float) var jump_impulse_velocity = 12
-export(float) var air_acceleration = 15
-export(float) var ground_acceleration = 45
-export(float) var gravity_constant = 25
-export(float) var max_run_velocity = 12.0
-export(float) var max_walk_velocity = 6.0
-export(float) var max_velocity = 30.0 # meter per second
+export(bool) var AUTO_BHOP = false
+export(float) var CROUCH_SPEED = 3 # meter per second
+export(float) var JUMP_IMPULSE_VELOCITY = 12
+export(float) var AIR_ACCELERATION = 20
+export(float) var GROUND_ACCELERATION = 80
+export(float) var GRAVITY_CONSTANT = 25
+export(float) var MAX_RUN_VELOCITY = 13.0
+export(float) var MAX_WALK_VELOCITY = 6.0
+export(float) var MAX_VELOCITY = 30.0 # meter per second
 
 ###########################################################
 # Engine Callbacks
 ###########################################################
 
+# BUG: the screen is jittery because render and physics are done in two different FPS
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	# height of the character from local origin + the crouch height delta with original height
+	var body_height = $Body.shape.height
+	headlimit_raycast.cast_to = Vector3.UP * (body_height - crouch_height)
 
 
 func _unhandled_input(event):
@@ -68,8 +74,7 @@ func _physics_process(delta: float) -> void:
 	# TODO-BIG: refactor input so that it would be outside of this script
 	#           this will ensure possibilities for multiplayer in the future
 	manage_crouching(delta)
-	apply_movement(get_movement_input($Pivot/Camera.global_transform.basis), delta)
-
+	handle_movement(get_movement_input($Pivot/Camera.global_transform.basis), delta)
 
 ###########################################################
 # Stateful function
@@ -81,53 +86,56 @@ func _physics_process(delta: float) -> void:
 ### This process is reversible because of how crouching works which is:
 ###     * Crouch
 ###     * Come to normal
-### BUG: shouldn't exit crouch when clearence is standing limit
+### BUG: if crouching under moving platform, player would stand up and down repeatedly
 func manage_crouching(delta: float):
-	if Input.is_action_pressed("player_crouch"):
-		is_crouching = true
+	if Input.is_action_pressed("player_crouch") or $Body.shape.height < body_original_height:
+		self.is_crouching = true
 
+	if not Input.is_action_pressed("player_crouch") and is_crouching and not headlimit_raycast.is_colliding():
+		is_crouching = false
+
+	if is_crouching:
 		var height_change = abs(crouch_height - body_original_height)
 		var body_trans_target = body_original_local_translation + Vector3.UP * height_change / 2
 		var feet_trans_target = feet_original_local_translation + Vector3.UP * height_change
 
-		$Body.shape.height = move_toward($Body.shape.height, crouch_height, crouch_speed * delta)
-		$Body.transform.origin = $Body.transform.origin.move_toward(body_trans_target, crouch_speed * delta)
-		$Feet.transform.origin = $Feet.transform.origin.move_toward(feet_trans_target, crouch_speed * delta)
+		$Body.shape.height = move_toward($Body.shape.height, crouch_height, CROUCH_SPEED * delta)
+		$Body.transform.origin = $Body.transform.origin.move_toward(body_trans_target, CROUCH_SPEED * delta)
+		$Feet.transform.origin = $Feet.transform.origin.move_toward(feet_trans_target, CROUCH_SPEED * delta)
 	else:
-		is_crouching = false
-
-		var crouch_delta = crouch_speed * 1.5 * delta
+		var crouch_delta = CROUCH_SPEED * 1.5 * delta
 		$Body.shape.height = move_toward($Body.shape.height, body_original_height, crouch_delta)
 		$Body.transform.origin = $Body.transform.origin.move_toward(body_original_local_translation, crouch_delta)
 		$Feet.transform.origin = $Feet.transform.origin.move_toward(feet_original_local_translation, crouch_delta)
 
-
-func apply_movement(input_vector: Vector3, delta: float):
+# TODO: decouple these codes into other smaller more concise function
+func handle_movement(input_vector: Vector3, delta: float):
 	input_vector = input_vector.normalized()
 
 	# slide the input_vector so that it would be on the plane in which it walks
 	var input_slanted = input_vector
 	if is_on_floor():
 		input_slanted = input_vector.slide(get_floor_normal()).normalized()
-
-	if not is_on_floor():
+	if is_on_ceiling():
+		gravity_velocity = Vector3.ZERO
+	elif is_on_floor():
+		gravity_velocity = -self.get_floor_normal() * MAGIC_ON_GROUND_GRAVITY
+		current_acceleration = GROUND_ACCELERATION
+	else:
 		gravity_velocity.x = 0
 		gravity_velocity.z = 0
-		gravity_velocity += Vector3.DOWN * (gravity_constant * delta)
-		current_acceleration = air_acceleration
-	else:
-		gravity_velocity = -self.get_floor_normal() * MAGIC_ON_GROUND_GRAVITY
-		current_acceleration = ground_acceleration
+		gravity_velocity += Vector3.DOWN * (GRAVITY_CONSTANT * delta)
+		current_acceleration = AIR_ACCELERATION
 
-	# Player walk action that will decrease max_velocity
-	if Input.is_action_pressed("player_walk") and is_crouching:
-		current_max_movement_velocity = max_walk_velocity * 0.5
-	elif is_crouching:
-		current_max_movement_velocity = max_walk_velocity * 0.75
-	elif Input.is_action_pressed("player_walk"):
-		current_max_movement_velocity = max_walk_velocity
+	# Player walk action that will decrease MAX_VELOCITY
+	if Input.is_action_pressed("player_walk") and is_crouching and is_on_floor():
+		current_max_movement_velocity = MAX_WALK_VELOCITY * 0.5
+	elif is_crouching and is_on_floor():
+		current_max_movement_velocity = MAX_WALK_VELOCITY * 0.75
+	elif Input.is_action_pressed("player_walk") and is_on_floor():
+		current_max_movement_velocity = MAX_WALK_VELOCITY
 	else:
-		current_max_movement_velocity = max_run_velocity
+		current_max_movement_velocity = MAX_RUN_VELOCITY
 
 	# counterstrafing, by gradualy increasing/decreasing acceleration
 	if desired_movement_velocity.normalized().dot(input_slanted) < 0:
@@ -141,22 +149,28 @@ func apply_movement(input_vector: Vector3, delta: float):
 	#
 	# EXAMPLE: if the desired velocity isn't changed, player who went up a slope
 	#          will have higher jumping velocity than the one going downwards.
-	if is_on_floor() and (Input.is_action_just_pressed("player_jump") or (auto_bhop and Input.is_action_pressed("player_jump"))):
-		gravity_velocity = Vector3.UP * jump_impulse_velocity
+	if is_on_floor() and (Input.is_action_just_pressed("player_jump") or (AUTO_BHOP and Input.is_action_pressed("player_jump"))):
+		gravity_velocity = Vector3.UP * JUMP_IMPULSE_VELOCITY
 		desired_movement_velocity = input_vector * current_max_movement_velocity
+		desired_movement_velocity = Util.clamp_vector3(desired_movement_velocity, final_velocity.length())
 
 	# finalize to current velocity
-	# BUG: desired_movement_velocity need to be clamped when going against a wall
 	var velocity_and_gravity = desired_movement_velocity + gravity_velocity
 
 	# clamp the velocity and gravity to max speed
-	velocity_and_gravity = Util.clamp_vector3(velocity_and_gravity, max_velocity)
+	velocity_and_gravity = Util.clamp_vector3(velocity_and_gravity, MAX_VELOCITY)
 
 	# apply to move and slide
 	final_velocity = self.move_and_slide(velocity_and_gravity, Vector3.UP, true, 4, 0.785398, false)
 
-	# DEBUG
+	# DEBUG for interpolating
+	debug_position_one_frame_ago = global_transform.origin
+	print(str(Engine.get_physics_frames()) + " FRAME RIGHT NOW -> " + str(debug_position_one_frame_ago))
+	print(str(Engine.get_physics_frames()) + " FRAME LATER     -> " + str(global_transform.origin + (final_velocity * delta)))
+	# Engine.get_physics_interpolation_fraction()
+
 	State.change_state("DEBUG_PLAYER_VELOCITY", stepify(final_velocity.length(), 0.01))
+	State.change_state("DEBUG_MISC", str(desired_movement_velocity.length()))
 
 ###########################################################
 # Stateless Function
