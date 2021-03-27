6790457415bb2429b4fc6ed20e674c1ef67a86e2
tengkuizdihar <tengkuizdihar@gmail.com>
2021-03-27 08:26:34 +0700

    change the map a bit with shooting gallery now

--- compegodot/world_item/RandomShootingGallery.gd
+++ compegodot/world_item/RandomShootingGallery.gd
@@ -1,5 +1,5 @@
 tool
-extends Spatial
+extends Area
 
 # Target that's going to be randomly spawned in the level
 export (PackedScene) var target_scene
@@ -19,10 +19,30 @@ func _ready() -> void:
 				collision_area = i
 				break
 
+	spawn_target(collision_area.global_transform.origin)
+
+
+func spawn_target(target_origin: Vector3 = Vector3.INF) -> void:
+	var new_target = target_scene.instance() as Node
+	add_child(new_target)
+
+	if target_origin == Vector3.INF:
+		new_target.global_transform.origin = get_random_points_inside_area()
+	else:
+		new_target.global_transform.origin = target_origin
+
+	var interface = Util.get_interface(new_target, IHealth) as IHealth
+	interface.connect("dead", self, "_on_Target_dead")
+
+
 func get_random_points_inside_area() -> Vector3:
 	var shape = collision_area.shape as BoxShape
-	# TODO get random point inside of the box transformed by collision area transform
-	return Vector3()
+	var extents = shape.extents
+	var collision_area_glob_trans = collision_area.global_transform
+	var random_in_box = Vector3(rand_range(-extents.x, extents.x), rand_range(-extents.y, extents.y), rand_range(-extents.z, extents.z))
 
-# TODO in ready, spawn and connect the dead signal in targets
-# TODO set the spawn position to get_random_points_inside_area()
+	return collision_area_glob_trans.xform(random_in_box)
+
+func _on_Target_dead() -> void:
+	# spawn one more
+	spawn_target()
